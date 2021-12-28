4035e69f69b0a35d2fc3b0b4bdb2ca3dd6b3c739
cem tugut <cem.tugut@hotmail.com.tr>
2021-12-28 14:19:12 +0300

    removed coinbasecommerce index page

--- src/Goodnites.Payment.CoinbaseCommerce.Web/Pages/CoinbaseCommerce/Index.cshtml.cs
+++
@@ -1,9 +0,0 @@
-namespace Goodnites.Payment.CoinbaseCommerce.Web.Pages.CoinbaseCommerce
-{
-    public class IndexModel : CoinbaseCommercePageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
-}
